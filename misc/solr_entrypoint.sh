be3fe87e2db1f651b0b696d4a32d8e87f85f014c
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-03-24 22:10:46 +0100

    Make Solr start version-independent

--- misc/solr_entrypoint.sh
+++ misc/solr_entrypoint.sh
@@ -3,4 +3,4 @@
 precreate-core stable /owdex_configset
 precreate-core unstable /owdex_configset
 precreate-core archive /owdex_configset
-/opt/solr-9.1.1/bin/solr start -f
+/opt/solr/bin/solr start -f
