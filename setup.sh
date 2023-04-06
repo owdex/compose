cb10b9ae072b1f7f8201b3560ef71e4bf4939daa
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-04-06 17:24:11 +0200

    Make default config files and ignore actual ones

--- setup.sh
+++ setup.sh
@@ -18,6 +18,10 @@ mkdir -p ./data/mongo
 echo "Setting permissions..."
 chown -R 8983 ./data/solr
 
+echo "Creating config files..."
+cp owdex.toml.default owdex.toml
+cp misc/Caddyfile.default misc/Caddyfile
+
 echo "All done. Happy searching!"
 
 echo "Note that you MUST edit ./owdex.toml and ./misc/Caddyfile. If you don't, not only will your install be unsecured, it won't work."
